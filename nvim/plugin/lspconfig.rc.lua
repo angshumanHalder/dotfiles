624dfdcf232b45c5c680445f5181b7f7d002e456
Angshuman Halder <angshuman.halder243@gmail.com>
2023-05-21 00:22:39 +0530

    eslint settings

--- nvim/plugin/lspconfig.rc.lua
+++ nvim/plugin/lspconfig.rc.lua
@@ -90,6 +90,39 @@ nvim_lsp.eslint.setup {
       command = "EslintFixAll",
     })
   end,
+  settings = {
+    codeAction = {
+      disableRuleComment = {
+        enable = true,
+        location = "separateLine"
+      },
+      showDocumentation = {
+        enable = true
+      }
+    },
+    codeActionOnSave = {
+      enable = false,
+      mode = "all"
+    },
+    experimental = {
+      useFlatConfig = false
+    },
+    format = false,
+    nodePath = "",
+    onIgnoredFiles = "off",
+    packageManager = "npm",
+    problems = {
+      shortenToSingleLine = false
+    },
+    quiet = false,
+    rulesCustomizations = {},
+    run = "onType",
+    useESLintClass = false,
+    validate = "on",
+    workingDirectory = {
+      mode = "location"
+    }
+  },
 }
 
 nvim_lsp.lua_ls.setup {
