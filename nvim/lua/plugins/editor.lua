4bfa5de0d9daa3ce5ff4cb143d227136763baca9
Angshuman Halder <angshuman.halder243@gmail.com>
2024-01-20 00:30:14 +0530

    Show hidden files

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -198,6 +198,20 @@ return {
     "nvim-neo-tree/neo-tree.nvim",
     cmd = "Neotree",
     opts = {
+      filesystem = {
+        filtered_items = {
+          visible = true,
+          show_hidden_count = true,
+          hide_dotfiles = false,
+          hide_gitignored = true,
+          hide_by_name = {
+            -- '.git',
+            -- '.DS_Store',
+            -- 'thumbs.db',
+          },
+          never_show = {},
+        },
+      },
       window = {
         mappings = {
           ["o"] = "open",
