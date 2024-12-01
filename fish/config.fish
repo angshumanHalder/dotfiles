5767d2d95e5dd1664bfc77b8df6ccc836d4a3083
Angshuman Halder <angshuman.halder243@gmail.com>
2024-12-02 01:08:40 +0530

    karabiner config

--- fish/config.fish
+++ fish/config.fish
@@ -7,5 +7,4 @@ set -gx EDITOR nvim
 set -gx LC_ALL C
 set -gx LC_ALL en_US.UTF-8
 
-test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
-
+test -e {$HOME}/.iterm2_shell_integration.fish; and source {$HOME}/.iterm2_shell_integration.fish
