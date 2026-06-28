8034ea2b1d113e537914fe4800a920838b4a75cb
Angshuman Halder <angshuman.halder243@gmail.com>
2026-06-28 22:00:03 +0530

    fix: correct golangci-lint linter name for nvim-lint

--- nvim/init.lua
+++ nvim/init.lua
@@ -561,7 +561,7 @@ lint.linters_by_ft = {
 	javascript = { "eslint_d" },
 	typescript = { "eslint_d" },
 	typescriptreact = { "eslint_d" },
-	go = { "golangci_lint" },
+	go = { "golangcilint" },
 }
 
 vim.api.nvim_create_autocmd({ "BufWritePost", "BufReadPost", "InsertLeave" }, {
